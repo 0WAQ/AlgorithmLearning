78f6576c847d1a6648dceb911f876f35e1929d1d
bwb_arch <2758282037@qq.com>
2023-10-27 20:05:01 +0800

    aa

---
+++ list/class/lc206.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ListNode* reverseList(ListNode* head) 
+{
+
+}
+
+int main()
+{
+    ListNode* temp;
+
+    
+
+    return 0;
+}
