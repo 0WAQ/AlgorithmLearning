561b5633784404a8963b4b698d3ceaac85eb9718
bwb <2758282037@qq.com>
2023-10-29 16:30:26 +0800

    README.md && list:lc206

--- list/class/lc206.cpp
+++ list/class/lc206.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+
+};
 
 ListNode* reverseList(ListNode* head) 
 {
-
+    ListNode* l = nullptr, *r = head;
+    while (r)
+    {
+        ListNode* temp = r->next;
+        r->next = l;
+        l = r, r = temp;
+    }
+    return l;
 }
 
 int main()
 {
-    ListNode* temp;
+    int n, x;
+    cin >> n;
+    ListNode* l = nullptr, *r = nullptr;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x;
+        ListNode* s = new ListNode;
+        s->val = x;
+        s->next = nullptr;
+        if(!i)
+            l = r = s;
+        else
+        {
+            r->next = s;
+            r = s;  
+        }    
+    }
 
-    
+    l = reverseList(l);
 
+    while(l)
+    {
+        cout << l->val << ' ';
+        l = l->next;
+    }
+    cout << endl;
     return 0;
 }
