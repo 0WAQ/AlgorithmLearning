10ade0f174ec2773c5f5a87a274f222806086f80
bwb_arch <2758282037@qq.com>
2023-11-05 14:10:45 +0800

    p1016

--- greedy/ex/p1016.cpp
+++ greedy/ex/p1016.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <functional>
 using namespace std;
 
@@ -33,9 +34,11 @@ int main()
         cin >> x >> y;
         a.push_back(node(x, y));
     }
-
+    a.push_back(node(d1, 0)); // 最后一个油站是目的地
     double c0 = 0; // 当前汽车的油量
-    for (auto i = a.begin(); i != a.end(); i++)
+
+    auto i = a.begin();
+    while(i != a.end())
     {
         // 寻找下一个油价小于等于当前油价的油站（中间油站的油价一定大于该油站）
         auto tar = find_if(i + 1, a.end(), [i](node t)
@@ -47,26 +50,55 @@ int main()
             // 那么加油直到刚好可以到该油站（不加油）
             auto c1 = (tar->d - i->d) / d2;           // 需要的油量
             ans += (c0 >= c1 ? 0 : (c1 - c0) * i->p); // 计算油费
+            i = tar;    // 走到该油站
         }
         else // 否则到不了该油站或者没有更便宜的油站
         {
             // 那么加满油（因为中间的油站的油价一定大于当前油价）
+
             ans += (c - c0) * i->p; // 计算油费
             c0 = c;                 // 加满油
 
             // 找到最远能够到达的油站
+
             double m = c * d2 + i->d; // 最远能够到达的距离（距离出发点的）
-            // 找到最后一个小于等于m的油站(第一个大于等于m的油站,注意边界)，即为最远油站
-            auto md = find_if(i + 1, a.end(), [i](node t)
-                              { return t.d >= i->d; });
+            
+                // 找到最后一个小于等于m的油站(第一个大于等于m的油站,注意边界)，即为最远油站
+            auto md = find_if(i + 1, a.end(), [m](node t)
+                              { return t.d >= m; });
+
             if (md->d != m) // x ---- m(如果没有等于m的油站) ---- y，那么md应该--
                 md--;
+
             // 从当前油站与最远油站之间找到一个油价最便宜的油站
             tar = min_element(i + 1, md + 1);
-            // 走到改该油站
+
+            // 如果加满油也到不了该油站,那么直接No Solution
+            if(m < tar->d)
+                break;
+
+            // 走到该油站
             c0 -= (tar->d - i->d) / d2;
+            i = tar;
         }
     }
-    printf("%.2lf\n", ans);
+    if(i == a.end())
+        printf("%.2lf\n", ans);
+    else
+        cout << "No Solution" << endl;
     return 0;
 }
+
+/*
+
+475.6 11.9 27.4 14.98 6
+102.0 9.99 -
+220.0 13.29
+256.3 14.79
+275.0 10.29
+277.6 11.29
+381.8 10.09
+
+
+192.15
+*/
