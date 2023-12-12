0100fd0d2565b9342deb2fe48446d94994c6f789
bwb <2758282037@qq.com>
2023-12-12 20:01:35 +0800

    12.12

---
+++ path/class/p4779.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+using PII = pair<int, int>;
+
+priority_queue<PII, vector<PII>, greater<PII>> q;
+const int N = 1e5 + 10, M = 2e5 + 10;
+int h[N], e[M], ne[M], w[M], idx;
+int dist[N];
+bool st[N];
+int n, m, s;
+
+void add(int a, int b, int c)
+{
+    w[idx] = c, e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+void dijkstra()
+{
+    memset(dist, 0x3f, sizeof(dist));
+    
+    dist[s] = 0;
+    q.push({0, s});
+    
+    while(q.size())
+    {
+        auto t = q.top();
+        q.pop();
+
+        int ver = t.second, distance = t.first;
+        
+        if(st[ver]) continue;
+        st[ver] = true;
+        
+        for(int i = h[ver]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if(distance + w[i] < dist[j])
+            {
+                dist[j] = distance + w[i];
+                q.push({dist[j], j});
+            }
+        }
+    }
+}
+
+int main()
+{
+    memset(h, -1, sizeof h);
+    cin >> n >> m >> s;
+    while(m--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        add(a, b, c);
+    }
+    dijkstra();
+    for(int i = 1; i <= n; i++)
+        cout << dist[i] << ' ';
+    cout << endl;
+    return 0;
+}
