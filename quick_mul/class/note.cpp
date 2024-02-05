ccf680f3f9ef7e5fa5ea3c9eab8f70f7c246bd39
bwb <2758282037@qq.com>
2024-02-05 23:17:00 +0800

    2.5

--- quick_mul/class/note.cpp
+++ quick_mul/class/note.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 using namespace std;
 
+const int mod = 10000007;
 int n, x, ans = 1;
 
 int main()
@@ -12,9 +13,9 @@ int main()
     cin >> n >> x;
     while(x)   
     {
-        if(x % 2)   ans *= n;   // 该位为1
-        n *= n;
-        x /= 2;
+        if(x & 1)   ans *= n % mod;   // 该位为1
+        n *= n % mod;
+        x >>= 1;
     }  
     cout << ans << endl;
     return 0;
